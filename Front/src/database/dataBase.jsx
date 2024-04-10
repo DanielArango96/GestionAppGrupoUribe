d1174420c22e28b147d5106f73b9a1c76c9cf794
Juan Pablo <jguevara@cesde.net>
2024-04-10 16:34:27 -0500

    se actualiza 10-04

---
+++ Front/src/database/dataBase.jsx
@@ -0,0 +1,13 @@
+export let usuarios= [
+    {
+        usuario:"admin",
+        contrasena: "1234",
+        correo: "jdas2401@gmail.com"
+    },
+
+    {
+        usuario:"adminsuper",
+        contrasena: "1234",
+        correo: "jdas2401@gmail.com"
+    },
+];
